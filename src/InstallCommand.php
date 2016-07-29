97d43dc225fb530493fbef310a6d1eac65764239
matthijs110 <msin2@me.com>
2016-07-29 11:16:22 +0200

    PSR-4 update

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App;
-
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
